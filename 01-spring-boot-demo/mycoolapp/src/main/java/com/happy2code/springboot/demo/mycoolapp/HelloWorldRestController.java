f3263036b39341ce604e42bc2e79f3d8ca8c4f2a
Tommy <thantzinmin.us@gmail.com>
2025-09-01 21:40:04 +0700

    feature: hello world rest controller

---
+++ 01-spring-boot-demo/mycoolapp/src/main/java/com/happy2code/springboot/demo/mycoolapp/HelloWorldRestController.java
@@ -0,0 +1,14 @@
+package com.happy2code.springboot.demo.mycoolapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    // Expose "/" that returns Hello World
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
