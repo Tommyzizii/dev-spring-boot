1f325b81066b09ba648f3dc1c9a7b24a86033933
Tommy <thantzinmin.us@gmail.com>
2025-09-06 17:24:56 +0700

    learn: configuration @Value for custom properties

--- 01-spring-boot-overview/06-properties-demo/mycoolapp/src/main/java/com/happy2code/springboot/demo/mycoolapp/rest/HelloWorldRestController.java
+++ 01-spring-boot-overview/06-properties-demo/mycoolapp/src/main/java/com/happy2code/springboot/demo/mycoolapp/rest/HelloWorldRestController.java
@@ -1,10 +1,23 @@
-package com.happy2code.springboot.demo.mycoolapp;
+package com.happy2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldRestController {
+
+    // Injecting Custom properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
 
     // Expose "/" that returns Hello World
     @GetMapping("/")
