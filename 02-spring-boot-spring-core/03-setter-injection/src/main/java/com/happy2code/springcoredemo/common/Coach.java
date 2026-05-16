31dcb4b029bca522228954c656c691d614d7e678
Tommy <thantzinmin.us@gmail.com>
2026-05-16 17:37:07 +0700

    learn: setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/happy2code/springcoredemo/common/Coach.java
@@ -0,0 +1,12 @@
+package com.happy2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+public interface Coach {
+
+    String getDailyWorkout();
+
+    String getDailyDietPlan();
+
+    String getDailyBMI();
+}
---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/happy2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,31 @@
+package com.happy2code.springcoredemo.rest;
+
+import com.happy2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach coach)
+    {
+        myCoach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/dailybmi")
+    public String getDailyBMI(){
+        return myCoach.getDailyBMI();
+    }
+
+
+}
