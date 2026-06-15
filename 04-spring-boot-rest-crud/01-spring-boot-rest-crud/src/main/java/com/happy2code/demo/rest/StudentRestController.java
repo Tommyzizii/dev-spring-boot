35f0c999216b0bf5a8abc5e88ad2d2ae379a9415
Tommy <thantzinmin.us@gmail.com>
2026-06-15 16:32:35 +0700

    learn: @PathVariable

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/happy2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/happy2code/demo/rest/StudentRestController.java
@@ -4,25 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.happy2code.demo.entity.Coffee;
 
+import jakarta.annotation.PostConstruct;
+
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
-	@GetMapping("/coffee")
-	public List<Coffee> getCoffee(){
 	
-		List<Coffee> myCoffee = new ArrayList();
-		
+	private List<Coffee> myCoffee = new ArrayList<>();
+	
+	@PostConstruct
+	public void loadData() {
 		myCoffee.add(new Coffee("Signature Iced Coffee", "True", "50%"));
 		myCoffee.add(new Coffee("Signature Iced Coffee", "Punthai", "25%"));
 		myCoffee.add(new Coffee("Iced Latte", "Hugo Brew", "50%"));
 		
+	}
+	
+
+	@GetMapping("/coffee")
+	public List<Coffee> getCoffee(){		
 		return myCoffee;
 	}
+	
+	@GetMapping("/coffee/{coffeeId}")
+	public Coffee getTheCoffee(@PathVariable int coffeeId) {
+		
+		return myCoffee.get(coffeeId);
+	}
+	
+	
 }
