2e2d3c06428a54b02bdb61a7643052221bcf4ba1
Tommy <thantzinmin.us@gmail.com>
2026-06-19 00:09:43 +0700

    Project setup

--- 04-spring-boot-rest-crud/spring-boot-employee-sql-script/employee-directory.sql
+++ 04-spring-boot-rest-crud/spring-boot-employee-sql-script/employee-directory.sql
@@ -20,9 +20,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'Leslie','Andrews','leslie@happy2code.com'),
+	(2,'Emma','Baumgarten','emma@happy2code.com'),
+	(3,'Avani','Gupta','avani@happy2code.com'),
+	(4,'Yuri','Petrov','yuri@happy2code.com'),
+	(5,'Juan','Vega','juan@happy2code.com');
 
