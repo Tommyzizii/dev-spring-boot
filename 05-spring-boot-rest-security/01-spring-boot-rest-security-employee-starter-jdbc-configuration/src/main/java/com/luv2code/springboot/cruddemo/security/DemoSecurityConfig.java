777605627d8bad4cd070ea8ceba491449de5773c
Tommy <thantzinmin.us@gmail.com>
2026-06-25 15:06:56 +0700

    learn: spring security jdbc configuration with plain text{noop}

---
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security-employee-starter-jdbc-configuration/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class DemoSecurityConfig {
+	
+	// add support for JDBC ... no more mock data 
+	
+	@Bean
+	public UserDetailsManager userDetailsManger(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(configurer -> 
+		configurer.requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+				  .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+				  .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+				  .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+				  .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+				  .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+		);
+		
+		// use HTTP Basic Authentication
+		http.httpBasic(Customizer.withDefaults());
+		
+		// disable Cross Site Request Forgery(CSRF)
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
